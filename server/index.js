7134f0feb78443c50cb52dc8d6f3f6b1fc32fd89
Fatih EGE <ifatxh@gmail.com>
2023-07-22 13:59:54 +0300

    Add a middleware for checking host IP

--- server/index.js
+++ server/index.js
@@ -10,12 +10,18 @@ import userRoutes from './routes/user.js'
 import trackRoutes from './routes/track.js'
 import checkDir from './utils/check-dir.js'
 import {__dirname} from './utils/dirname.js'
+import config from './config.js'
 
 const app = express() // Create Express server instance
 
 app.use(cors())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+    if (req.ip && config.allowedIps.includes(req.ip)) next() // If the host IP is allowed, continue
+    else return res.status(401).send() // Otherwise, return 401 response
+})
 
 app.get('/', (req, res) => {
     res.status(200).json({
@@ -25,18 +31,18 @@ app.get('/', (req, res) => {
 })
 
 app.get('/image/:image', (req, res) => {
-    const {image} = req.params
-    const dir = join(__dirname, '..', 'images')
-    const imagePath = join(dir, image)
+    const {image} = req.params // Get file name from request parameters
+    const dir = join(__dirname, '..', 'images') // Get path to the images directory
+    const imagePath = join(dir, image) // Get path to the image
 
-    checkDir(dir)
+    checkDir(dir) // If images directory is not exist, create it
 
-    if (!fs.existsSync(imagePath)) return res.status(404).json({
+    if (!fs.existsSync(imagePath)) return res.status(404).json({ // If image is not exist, return 404 response
         status: 'NOT FOUND',
     })
 
-    const stream = fs.createReadStream(imagePath)
-    stream.pipe(res)
+    const stream = fs.createReadStream(imagePath) // Create read stream for image
+    stream.pipe(res) // Pipe read stream into response
 })
 
 app.use('/user', userRoutes) // Use user routes in /user endpoint
